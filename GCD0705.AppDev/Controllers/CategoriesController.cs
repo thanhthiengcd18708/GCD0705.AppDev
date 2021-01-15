51cab754c3a06e0edf10c9c2831060805a1c9b16
Vinh Hoang Nhu (FE FGW DN) <VinhHN3@fe.edu.vn>
2021-01-15 15:35:10 +0700

    EXERCISE: Implement CategoriesController and Views

---
+++ GCD0705.AppDev/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using GCD0705.AppDev.Models;
+using System.Web.Mvc;
+
+namespace GCD0705.AppDev.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		// GET: Categories
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			// Todo
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Category category)
+		{
+			// Todo
+			return View();
+		}
+
+		public ActionResult Delete(int id)
+		{
+			// Todo
+			return View();
+		}
+
+		[HttpGet]
+		public ActionResult Edit(int id)
+		{
+			// Todo
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Category category)
+		{
+			// Todo
+			return View();
+		}
+	}
+}
