bdc257b34b8b7bdb066e4034151a7016a386d0ad
Vinh Hoang Nhu (FE FGW DN) <VinhHN3@fe.edu.vn>
2021-01-20 15:24:05 +0700

    Add Profiles action of Admins

---
+++ GCD0705.AppDev/Controllers/AdminsController.cs
@@ -0,0 +1,40 @@
+﻿using GCD0705.AppDev.Models;
+using GCD0705.AppDev.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GCD0705.AppDev.Controllers
+{
+	[Authorize(Roles = "admin")]
+	public class AdminsController : Controller
+	{
+		private ApplicationDbContext _context;
+		private UserManager<ApplicationUser> _userManager;
+		public AdminsController()
+		{
+			_context = new ApplicationDbContext();
+			_userManager = new UserManager<ApplicationUser>(
+				new UserStore<ApplicationUser>(new ApplicationDbContext())
+			);
+		}
+		// GET: Admins
+		public ActionResult Profiles()
+		{
+			var users = _context.Users.ToList();
+			var usersWithRole = new List<UsernameWithRoleViewModel>();
+
+			foreach (var user in users)
+			{
+				usersWithRole.Add(new UsernameWithRoleViewModel()
+				{
+					Username = user.UserName,
+					Rolename = _userManager.GetRoles(user.Id)[0]
+				});
+			}
+			return View(usersWithRole);
+		}
+	}
+}
---
+++ GCD0705.AppDev/ViewModels/UsernameWithRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GCD0705.AppDev.ViewModels
+{
+	public class UsernameWithRoleViewModel
+	{
+		public string Username { get; set; }
+		public string Rolename { get; set; }
+	}
+}
